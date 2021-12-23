939975be9435a82c7ccb19c922088ef43e511ffe
Marco Galvan <94484588+marGalvan98@users.noreply.github.com>
2021-12-23 19:58:25 -0300

    app terminada

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+// Para guardar en un "type" el tipado, asi no sea tan largo dentro de la function
+type FormElment = React.FormEvent<HTMLFormElement>;
 
-function App() {
+// Creo esta interface para poder setear el state
+// como un array de tareas
+interface ITasks {
+  name: string;
+  done: boolean;
+}
+
+function App(): JSX.Element {
+  let [newTask, setNewTask] = useState<string>("");
+  let [tasks, setTasks] = useState<ITasks[]>([]);
+  let taskInput = useRef<HTMLInputElement>(null);
+
+  function handleSubmit(e: FormElment): void {
+    e.preventDefault();
+    addTask(newTask);
+    setNewTask("");
+    taskInput.current?.focus();
+  }
+
+  const addTask = (name: string): void => {
+    let newTasks: ITasks[] = [...tasks, { name, done: false }];
+    setTasks(newTasks);
+  };
+
+  function toggleDoneTask(i: number): void {
+    const newTasks: ITasks[] = [...tasks];
+    newTasks[i].done = !newTasks[i].done;
+    setTasks(newTasks);
+  }
+
+  function removeTask(i: number): void {
+    const newTasks: ITasks[] = [...tasks];
+    newTasks.splice(i, 1);
+    setTasks(newTasks);
+  }
+
+  // Renderizado
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <input
+                  className="form-control"
+                  type="text"
+                  onChange={(e) => {
+                    setNewTask(e.target.value);
+                  }}
+                  value={newTask}
+                  ref={taskInput}
+                  autoFocus
+                />
+                <button className="btn btn-success btn-block mt-2">Save</button>
+              </form>
+            </div>
+          </div>
+          {tasks.map((t: ITasks, i: number) => {
+            return (
+              <div key={i} className="card card-body mt-2">
+                <h2 style={{ textDecoration: t.done ? "line-through" : "" }}>
+                  {t.name}
+                </h2>
+                <div className="btn btn-secondary">
+                  <button onClick={() => toggleDoneTask(i)}>
+                    {t.done ? "✓" : "✕"}
+                  </button>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => {
+                      removeTask(i);
+                    }}
+                  >
+                    🗑
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
